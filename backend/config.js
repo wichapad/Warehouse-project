ac040c5f839b120de7525cb788b86af0168a28e7
test-system-project <guy_wichapad@hotmail.com>
2024-07-04 17:49:31 +0700

    Update config database

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,11 @@
 const {  Pool } = require("pg");
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'warehouse',
-    password: 'passw0rd',
-    port: 5433,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 })
 
 module.exports = pool;
